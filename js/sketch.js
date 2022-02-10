318b24e53939ae6fba63e18cbfabbddca07a9c62
Joao-Gontijo <jvgontijo166@gmail.com>
2022-02-10 11:21:54 -0300

    positioning actors on screen

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,19 @@
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+
+function preload(){
+    imagemDaEstrada = loadImage("./assets/images/estrada.png");
+    imagemDoAtor = loadImage("./assets/images/ator-1.png");
+    imagemCarro = loadImage("./assets/images/carro-1.png");
+}
+
 function setup(){
-    createCanvas(400, 400);
+    createCanvas(600, 400);
 }
 
 function draw(){
-    background(220);
+    background(imagemDaEstrada);
+    image(imagemDoAtor, 100, 365, 30, 30);
+    image(imagemCarro, 400, 40, 60, 40);
 }
