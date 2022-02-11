de372136d6302f3af32ee7223f315943e81bc792
Joao-Gontijo <jvgontijo166@gmail.com>
2022-02-11 15:05:43 -0300

    adding other cars

--- js/carro.js
+++ js/carro.js
@@ -1,5 +1,11 @@
 //carro
 let xCarro = 600;
+let xCarro2 = 600;
+let xCarro3 = 600;
+
+let velocidadeCarro1 = 2;
+let velocidadeCarro2 = 3;
+let velocidadeCarro3 = 1.5;
 
 //posições da estrada
 const primeiraFaixa = 40;
@@ -11,9 +17,13 @@ const sextaFaixa = 320;
 
 function mostraCarro(){
     image(imagemCarro, xCarro, primeiraFaixa, 60, 40);
+    image(imagemCarro2, xCarro2, segundaFaixa, 60, 40);
+    image(imagemCarro3, xCarro3, terceiraFaixa, 60, 40);
     movimentaCarro();
 }
 
 function movimentaCarro(){
-    xCarro -= 2;
+    xCarro -= velocidadeCarro1;
+    xCarro2 -= velocidadeCarro2;
+    xCarro3 -= velocidadeCarro3;
 }
