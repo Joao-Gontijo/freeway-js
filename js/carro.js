e83222355abca09ea5921fa2ea522611992ae061
Joao-Gontijo <jvgontijo166@gmail.com>
2022-02-11 16:00:40 -0300

    creating loops

--- js/carro.js
+++ js/carro.js
@@ -1,34 +1,33 @@
 //carros
 let xCarros = [600, 600, 600];
 
-
 //posições da estrada
 let yCarros = [40, 97, 150, 211, 262, 320];
 
 let velocidadeCarros = [2, 3, 1.2]
 
 function mostraCarro(){
-    image(imagemCarro, xCarros[0], yCarros[0], 60, 40);
-    image(imagemCarro2, xCarros[1], yCarros[1], 60, 40);
-    image(imagemCarro3, xCarros[2], yCarros[2], 60, 40);
+    for(let i = 0; i < imagemCarros.length; i++){
+        image(imagemCarros[i], xCarros[i], yCarros[i], 60, 40); 
+    }
     movimentaCarro();
 }
 
 function movimentaCarro(){
-    xCarros[0] -= velocidadeCarros[0];
-    xCarros[1] -= velocidadeCarros[1];
-    xCarros[2] -= velocidadeCarros[2];
+    for(let i = 0; i < xCarros.length; i++){
+        xCarros[i] -= velocidadeCarros[i];
+    }
     voltaPosicaoInicialCarro();
 }
 
 function voltaPosicaoInicialCarro(){
-    if(xCarros[0] < -50){
-        xCarros[0] = 600;
+    for(let i = 0; i < xCarros.length; i++){
+        if(passouTodaATela(xCarros[i])){
+            xCarros[i] = 600;
+        }
     }
-    if(xCarros[1] < -50){
-        xCarros[1] = 600;
-    }
-    if(xCarros[2] < -50){
-        xCarros[2] = 600;
-    }
+}
+
+function passouTodaATela(xCarro){
+    return xCarro < -50;
 }
--- js/images.js
+++ js/images.js
@@ -4,11 +4,11 @@ let imagemCarro;
 let imagemCarro2;
 let imagemCarro3;
 
-
 function preload(){
     imagemDaEstrada = loadImage("./assets/images/estrada.png");
     imagemDoAtor = loadImage("./assets/images/ator-1.png");
     imagemCarro = loadImage("./assets/images/carro-1.png");
     imagemCarro2 = loadImage("./assets/images/carro-2.png");
     imagemCarro3 = loadImage("./assets/images/carro-3.png");
+    imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3];
 }
